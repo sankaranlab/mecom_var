8bc715c04d73520f79e8c4d19c426a46d78084b2
fy-yu <yufulong421@gmail.com>
2021-04-24 08:01:11 -0400

    strength_exp update

--- code/11-HSC_strength_exp.r
+++ code/11-HSC_strength_exp.r
@@ -10,8 +10,8 @@ library(data.table)
 setwd("/Users/fyu/Documents/GitHub/")
 
 
-load("mecom_var_sankaran/data/hememap/interaction_name_gene_info.rda")
-load("mecom_var_sankaran/data/hememap/01022021-heme_shortest_paths_geoActivate_info.rda")
+load("mecom_var_sankaran/data/hememap/interaction_name_gene_info.rda.temp")  # too lare to upload, please contact to the aunthors
+load("mecom_var_sankaran/data/hememap/01022021-heme_shortest_paths_geoActivate_info.rda.temp")  # too lare to upload, please contact to the aunthors
 expr_mat <- data.frame(fread("mecom_var_sankaran/data/hememap/10142020-expr_mat_16bulk.txt"))[, -1] # expr profiles; 20174
 tss250 <- data.frame(fread("mecom_var_sankaran/data/hememap/10142020-tss250.txt", header=F)) # tss of genes; 20174
 expr_idx <- rowSums(expr_mat)!=0 # remove 1682 genes were not expressed in all the cells
@@ -33,26 +33,35 @@ interaction_gene_name_group <- chunk2(as.vector(t(interaction_gene_name_sort)), 
 interaction_hsc_exp_group <- sapply(interaction_gene_name_group, function(x) expr_mat_cpm[match(x, tss250[, 4]), "HSC"] )
 sapply(interaction_hsc_exp_group, mean, na.rm=T)
 
-pdf("mecom_var_sankaran/data/exp_strength_cor/interaction_hsc_exp_group_box-nonrun_200_rev.pdf")
-boxplot(rev(interaction_hsc_exp_group), outline=FALSE, width=rep(0.4, 200), lwd=.5, 
-        # medcol = "indianred", medcex = 3,
-        ylab="gene expression in HSCs", col=rev(as.character(jdb_palette("brewer_spectra", type="continuous"))[200:1]))
-dev.off()
-
-
-
-# -------------- another visualization
-time_num <- 200
+# -------------- median visualization
+library(RColorBrewer)
+time_num <- 50
+set.seed(9527)
 interaction_gene_name_group <- chunk2(as.vector(t(interaction_gene_name_sort)), time_num)
 # interaction_gene_name_group <- sapply(interaction_gene_name_group, unique)
 interaction_hsc_exp_group <- sapply(interaction_gene_name_group, function(x) expr_mat_cpm[match(x, tss250[, 4]), "HSC"] )
-m <- sapply(interaction_hsc_exp_group, median, na.rm=T)  %>% rev() %>% barplot(., col=rev(as.character(jdb_palette("brewer_spectra", type="continuous"))[time_num:1]), ylim=c(0, 20))
+# m <- sapply(interaction_hsc_exp_group, median, na.rm=T)  %>% rev() %>% barplot(., col=rev(as.character(jdb_palette("brewer_spectra", type="continuous"))[time_num:1]), ylim=c(0, 20), border=NA)
+pdf("mecom_var_sankaran/data/exp_strength_cor/interaction_hsc_exp_strength_50_rev.pdf")
+xx <- jdb_palette("brewer_spectra", 4, type ="discrete")
+m <- sapply(interaction_hsc_exp_group, median, na.rm=T)  %>% 
+        rev() %>% 
+        barplot(., col=colorRampPalette(xx)(time_num)%>% rev(), ylim=c(0, 20), border=NA, ylab="Gene expression in HSCs", xlab="Increasing interaction strength", names.arg=NA)
+
 xx <- rep(0, time_num)
 for (i in 1:time_num){
         xx[i] <- expr_mat_cpm[sample(1:nrow(expr_mat_cpm), interaction_hsc_exp_group[[1]], replace=T), "HSC"] %>% median
 }
-points(m, xx, cex=.5)
-# -------------- z-score visualization
-(sapply(interaction_hsc_exp_group, median, na.rm=T)-mean(xx))/sd(xx)
-plot((sapply(interaction_hsc_exp_group, median, na.rm=T)-mean(xx))/sd(xx))
+points(m, xx, cex=1, lwd=2)
+dev.off()
+
+# # -------------- z-score visualization
+# (sapply(interaction_hsc_exp_group, median, na.rm=T)-mean(xx))/sd(xx)
+# plot((sapply(interaction_hsc_exp_group, median, na.rm=T)-mean(xx))/sd(xx))
+
+# # -------------- boxplot visualization
+# pdf("mecom_var_sankaran/data/exp_strength_cor/interaction_hsc_exp_group_box-nonrun_200_rev.pdf")
+# boxplot(rev(interaction_hsc_exp_group), outline=FALSE, width=rep(0.4, 200), lwd=.5, 
+#         # medcol = "indianred", medcex = 3,
+#         ylab="gene expression in HSCs", col=rev(as.character(jdb_palette("brewer_spectra", type="continuous"))[200:1]))
+# dev.off()
 
