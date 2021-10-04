54e9428c18b7d2a72e877febfd5c60e18ab67502
FLYu <57479051+fl-yu@users.noreply.github.com>
2021-10-04 14:56:39 -0400

    Update 24-sc_correlationwithHSCcompartment.sh

--- code/24-sc_correlationwithHSCcompartment.sh
+++ code/24-sc_correlationwithHSCcompartment.sh
@@ -1,6 +1,6 @@
 ######### 
 # fold change correlation for HSC compartment and all cells
-combineddeg <- read.csv("/Users/fyu/Library/Mobile Documents/com~apple~CloudDocs/Documents/Vijay/project/09022020-MECOM_Richard/data/20210801-cellrevision/all_cells_vs_lthsc.csv")
+combineddeg <- read.csv("all_cells_vs_lthsc.csv")
 # 2040   13
 sum(is.na(combineddeg$avg_logFC_x) | is.na(combineddeg$avg_logFC_y)) # 1316
 combineddeg <- combineddeg[!(is.na(combineddeg$avg_logFC_x) | is.na(combineddeg$avg_logFC_y)), ]
@@ -13,7 +13,6 @@ ggplot(combineddeg, aes(avg_logFC_x, avg_logFC_y))+
         geom_point() + theme_classic(base_size = 10) + xlim(-0.22, 0.4)  + xlab("log2(fold change) from all cells") +ylab("log2(fold change) from HSC compartment") +  #ylim(-1.7, 1) +
         ggrepel::geom_text_repel(data=eithertop5, aes(avg_logFC_x, avg_logFC_y, label = genename), color = 'black',
                         size = 2, max.overlaps=30, segment.linetype = 1)
-setwd("/Users/fyu/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/Vijay/project/09022020-MECOM_Richard/data/20210801-cellrevision")
 ggsave("foldchange_comparison_allcellandHSC.pdf", width = 5, height = 5)
 
 
